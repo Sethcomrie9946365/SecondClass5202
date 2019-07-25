0e149fac85a3347a78a3dca3e6277c8363b5bb84
Seth Comrie <9946365@student.toiohomai.ac.nz>
2019-07-25 13:23:58 +1200

    Task2

---
+++ SecondClassTask2/SecondClassTask2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondClassTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name? ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("what is your address? ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("How young are you? ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your e-mail? ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("What is your pin? ");
+            int pin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hey {0}, You live at {1}, you are only {2}, your e-mail is {3}, and your pin is: {4}", name, address, age, email,pin);
+            Console.ReadLine();
+            
+
+            
+        }
+    }
+}
