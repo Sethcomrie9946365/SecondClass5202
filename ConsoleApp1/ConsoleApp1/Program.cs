637fc585687a473c48070dea3dc6c64313404fae
Seth Comrie <9946365@student.toiohomai.ac.nz>
2019-08-15 12:00:44 +1200

    Updated

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your second name?");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Hello, {0} {1}, you're the man bro", name, lname);
+            Console.ReadLine();
+        }
+    }
+}
